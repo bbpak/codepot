405a88baa27f6fb8e6cb7499ac82d1ca6c55f917
bbpak <brianbpak@gmail.com>
2019-05-23 17:50:03 -0700

    use session storage to store repos so we don't fetch excessively

--- client/src/components/forms/ProjectForm.js
+++ client/src/components/forms/ProjectForm.js
@@ -4,13 +4,15 @@ import { Redirect } from 'react-router-dom'
 import { Form, Dropdown, Button } from 'semantic-ui-react'
 import ImageUploader from 'react-images-upload'
 import { camelCase } from 'lodash'
+import tags from '../../data/tags.json'
 import '../styles/form.css'
-
+	
 let reposData = {}
 
 const ProjectForm = (props) => {
 	const [ inputs, setInputs ] = useState({ user_id: props.currentUser.id })
 	const [ repoOptions, setRepoOptions ] = useState([])
+	const [ tagOptions, setTagOptions ] = useState([])
 	const [ selectedRepo, setSelectedRepo ] = useState(null)
 	const [ coverImage, setCoverImage ] = useState(null)
 	const [ isLoading, setIsLoading ] = useState(true)
@@ -31,44 +33,50 @@ const ProjectForm = (props) => {
 	}
 
 	useEffect(() => {
-		const reposURL = `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`
-		let repos = []
+		if (!sessionStorage.getItem("")) {
+			const reposURL = `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`
+			let repos = []
 
-		// Go through 300 repos, 100 per page limit
-		// For FI students with a ton of labs... Switch to GraphQL stretch goal
-		for (let i = 1; i <= 3; i++) {
-			// Fetch user's public github repos
-			fetch(reposURL + `&page=${i}`).then((res) => res.json()).then((data) => {
-				// For-loop instead of forEach so I can break
-				// Filter only relevant properties
-				for (let j = 0; j < data.length; j++) {
-					const repo = data[j]
+			// Go through 300 repos w/ 100 per page limit for FI students with a ton of labs...
+			// Switch to GraphQL stretch goal 🤞
+			for (let i = 1; i <= 3; i++) {
+				// Fetch user's public github repos
+				fetch(reposURL + `&page=${i}`).then((res) => res.json()).then((data) => {
+					// For-loop instead of forEach so I can break
+					// Filter only relevant properties
+					for (let j = 0; j < data.length; j++) {
+						const repo = data[j]
 
-					// Don't include forks
-					if (!repo.fork) {
-						if (repos.includes(repo)) break
+						// Don't include forks
+						if (!repo.fork) {
+							if (repos.includes(repo)) break
 
-						repos.push({
-							key: j,
-							text: repo.name,
-							value: repo.name
-						})
+							repos.push({
+								key: j,
+								text: repo.name,
+								value: repo.name
+							})
 
-						// To pre-fill form with selected repo
-						reposData[repo.name] = {
-							name: namify(repo.name),
-							repo_url: repo.html_url,
-							language: repo.language
+							// To pre-fill form with selected repo
+							reposData[repo.name] = {
+								name: namify(repo.name),
+								repo_url: repo.html_url,
+								language: repo.language
+							}
 						}
 					}
-				}
 
-				setRepoOptions(repos)
+					setRepoOptions(repos)
+					sessionStorage("repoOptions", repos)
 
-				// Done with all 3 fetch requests, set loading false
-				if (i === 3) setIsLoading(false)
-			})
+					// Done with all 3 fetch requests, set loading false
+					if (i === 3) setIsLoading(false)
+				})
+			}
 		}
+		
+
+
 	}, [])
 
 	const handleInputChange = (e) => {
@@ -110,15 +118,58 @@ const ProjectForm = (props) => {
 		})
 			.then((res) => res.json())
 			.then((data) => {
-				console.log(data)
 				setRedirect(true)
 			})
+	}
+
+	const renderFormField = (name) => {
+		return (
+		<Form.Field>
+			<Form.Input
+				label={namify(name)}
+				name={name}
+				value={inputs[name]}
+				onChange={handleInputChange}
+			/>
+		</Form.Field>)
+	}
+
+	const renderForm = () => {
+		return (
+			<>
+			<div className='project-form animated fadeInUp'>
+				<Form.Field className='project-form-image'>
+					<label className='label'>Cover Image</label>
+					<ImageUploader
+						withIcon
+						buttonText='Upload image'
+						onChange={handleImageDrop}
+						imgExtension={[ '.jpg', '.gif', '.png' ]}
+						maxFileSize={5242880}
+					/>
+				</Form.Field>
+				<div className='project-form-details'>
+					{renderFormField('name')}
+					{renderFormField('repo_url')}
+					{renderFormField('project_url')}
+				</div>
+			</div>
+			<Dropdown
+				fluid
+				multiple
+				search
+				selection
+				options={[{key:1, name: ''}]}
+			/>
+			</>
+		)
 	}
 
 	return (
 		<div className='project-form-container'>
 			<h1>New Project</h1>
 			<Dropdown
+				placeholder='repository'
 				onChange={handleDropdownChange}
 				value={selectedRepo}
 				options={repoOptions}
@@ -126,49 +177,8 @@ const ProjectForm = (props) => {
 				selection
 			/>
 			<Form onSubmit={handleSubmit}>
-				{selectedRepo && (
-					<div className='project-form animated fadeInUp'>
-						<Form.Field className='project-form-image'>
-							<label className='label'>Cover Image</label>
-							<ImageUploader
-								withIcon
-								buttonText='Upload image'
-								onChange={handleImageDrop}
-								imgExtension={[ '.jpg', '.gif', '.png' ]}
-								maxFileSize={5242880}
-							/>
-						</Form.Field>
-						<div className='project-form-details'>
-							<Form.Field>
-								<Form.Input
-									label='Project Name'
-									name='name'
-									placeholder='project name'
-									value={inputs['name']}
-									onChange={handleInputChange}
-								/>
-							</Form.Field>
-							<Form.Field>
-								<Form.Input
-									label='Repo URL'
-									name='repo_url'
-									placeholder='repository URL'
-									value={inputs['repo_url']}
-									onChange={handleInputChange}
-								/>
-							</Form.Field>
-							<Form.Field>
-								<Form.Input
-									label='Project URL'
-									name='project_url'
-									placeholder='project URL'
-									value={inputs['project_url']}
-									onChange={handleInputChange}
-								/>
-							</Form.Field>
-						</div>
-					</div>
-				)}
+				{selectedRepo && renderForm()}
+				
 				<div className='project-form-buttons'>
 					{redirect && <Redirect to='/' />}
 					<Button primary type='submit'>
